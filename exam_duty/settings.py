36f655f8f75414bcaf8d88ae952813624337b7ad
Krishana Kumar <your_email@example.com>
2026-03-11 12:07:23 +0530

    Render deploy setup

--- exam_duty/settings.py
+++ exam_duty/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'exam_duty.wsgi.application'
 # ---------------- DATABASE ----------------
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Use PostgreSQL for production
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': '5432',
     }
 }
 
